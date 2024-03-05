89713f0a0d015ffcdfb7dd7249d1992e43c118d7
shakibmojumderinfo <shakibmojumder99@gmail.com>
2024-03-05 22:07:51 +0600

    first commit

---
+++ main.js
@@ -0,0 +1,55 @@
+// signUpName = document.getElementById("signupName");
+// signUpEmail = document.getElementById("signupEmail")
+// signUpPass = document.getElementById("signupPass");
+// userEmail = document.getElementById('loginEmail');
+// userPassword = document.getElementById("loginPassword");
+// submitButton = document.getElementById("sub-btn");
+// existAcc = document.getElementById("existAcc");
+
+// function saveData(){
+//     let name, email, pass;
+
+//     name = document.getElementById("name").value;
+//     email = document.getElementById("email").value;
+//     pass = document.getElementById("pass").value;
+ 
+//     localStorage.setItem("name", name);
+//     localStorage.setItem("email", email);
+//     localStorage.setItem("pass", pass);
+
+    
+
+//     if (name && email && pass){
+        
+//         window.location.href = "home.html"
+//         alert("successfull sign up")
+//     }
+//     else {
+//         alert("Fill the requird fields");
+//     }
+// }
+
+
+// function signUp(){
+
+//     let name = signUpName.value;
+//     let email = signUpEmail.value;
+//     let pass = signUpPass.value;
+
+//     let user = 
+//     {
+        
+//         name : name,
+//         email : email,
+//         pass : pass
+//     };
+
+    // let json = JSON.stringify(user);
+    // localStorage.setItem("userAddress", json);
+    
+// }
+
+
+
+// setLocalStorageData(storageKey,AfterUpdateData);  
+//     window.location.reload();  
